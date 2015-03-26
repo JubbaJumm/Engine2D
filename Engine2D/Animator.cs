05b5d9c7f1b2b6cc0c879fd6cd660b02ef8150cf
Matthew R <matthewrz52@gmail.com>
2015-03-26 18:37:21 +1100

    Original commit.
    First commit.

---
+++ Engine2D/Animator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Engine2D
+{
+    public enum OffsetDirection
+    {
+        X,
+        Y
+    }
+    public class Animator
+    {
+        public Texture2D AnimationSheet;
+        public int SpriteWidth;
+        public int SpriteHeight;
+        public int SpriteSheetOffset;
+        public OffsetDirection OffsetDirection = OffsetDirection.X;
+        private int spritecount;
+
+        public Animator(Texture2D spritesheet, int spritewidth, int spriteheight, int spriteoffset, OffsetDirection direction = Engine2D.OffsetDirection.X)
+        {
+            AnimationSheet = spritesheet;
+            SpriteWidth = spritewidth;
+            SpriteHeight = spriteheight;
+            SpriteSheetOffset = spriteoffset;
+
+            spritecount = (OffsetDirection == OffsetDirection.X) ? (int)(AnimationSheet.Width / SpriteSheetOffset) : (int)(AnimationSheet.Height / SpriteSheetOffset);
+        }
+    }
+}
---
+++ Engine2D/Plane.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Engine2D
+{
+    public class Plane
+    {
+       //plane is just a 2d collisionbox, it can be non-solid as we will not do any collision checks on it
+    }
+}
